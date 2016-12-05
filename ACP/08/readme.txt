20193da7a20adfa7a1dac6d1d234cc3713bb1c63
praveenmhapsekar <praveenmhapsekar@gmail.com>
2016-12-04 22:24:43 -0800

    commit

--- ACP/08/readme.txt
+++ ACP/08/readme.txt
@@ -1,1 +1,11 @@
 
+Assignment 8
+
+Evaluate the validity of the scopes in the following expression implementing stacks.
+Ignore the operators and operand in the expression while checking the validity of the expression.
+
+Input Expression:
+{x + (y – [a + b] ) * (c – [ (d + e) ]  + k )} / (h – (j – (k – [l – n] ) ) ) + { u +  [m  * (p  – q)] / r }
+
+Print each scope or symbol encountered while scanning the expression and what resulting action taken for each 
+symbol input. Read all symbols from an input file and write the result to an output file.
--- LSP/05-Signal/Examples/7f/ex1.c
+++ LSP/05-Signal/Examples/7f/ex1.c
@@ -8,29 +8,28 @@
 #define TMOUT 5
 
 int
-main(void) {
+main(void) 
+{
     struct pollfd pollfd[1];
     int retval;
+    int c;
+    int myfd1=STDIN_FILENO;
 
-		int c;
+    memset(pollfd, 0,sizeof(struct pollfd));
+    pollfd->fd=myfd1;
+    pollfd->events |= POLLIN;
 
-		int myfd1=STDIN_FILENO;
+    fprintf(stderr,"Enter a char: (TIMEOUT=%d): ",TMOUT);
+    retval = poll(pollfd,1,TMOUT*1000); // 5s timeout
+    // retval = poll(pollfd,1,-1); // -1 signifies infinite timeout
 
-	 memset(pollfd, 0,sizeof(struct pollfd));
-   pollfd->fd=myfd1;
-	 pollfd->events |= POLLIN;
-
-	 fprintf(stderr,"Enter a char: (TIMEOUT=%d): ",TMOUT);
- 	 retval = poll(pollfd,1,TMOUT*1000); // 5s timeout
-// retval = poll(pollfd,1,-1); // -1 signifies infinite timeout
-
-   if (retval == -1)
+    if (retval == -1)
         perror("select()");
     else if (retval)
         printf("Data (#fd's=%d) is available now.\n", retval);
-				// (pollfd->revents & POLLIN)  or (pollfd->revents & POLLOUT)
+            // (pollfd->revents & POLLIN)  or (pollfd->revents & POLLOUT)
     else
         printf("No data within '%d' seconds.\n",TMOUT);
 
-   return 0;
+    return 0;
 }
