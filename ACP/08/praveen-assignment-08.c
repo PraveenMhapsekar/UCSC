592725c6cfe0d52296987dc08ccfb2b3557fd901
praveenmhapsekar <praveenmhapsekar@gmail.com>
2016-12-04 22:30:30 -0800

    commit

---
+++ ACP/08/praveen-assignment-08.c
@@ -0,0 +1,45 @@
+/******************************************************************************/
+/*****************************  prologue  *************************************/
+/*                                                                            */
+/*    University of California Extension, Santa Cruz                          */
+/*                                                                            */
+/*    Advanced C Programming                                                  */
+/*                                                                            */
+/*    Instructor: Rajainder A. Yeldandi                                       */
+/*                                                                            */
+/*    Author: Praveen Mhapsekar                                               */
+/*                                                                            */
+/*    Assignment Number: 08                                                   */
+/*                                                                            */
+/*    Topic: Chapter 07 Stack                                                 */
+/*                                                                            */
+/*    File name:  praveen-assignment-08.c                                     */
+/*                                                                            */
+/*    Date: 11/18/2016                                                        */
+/*                                                                            */
+/*    Objective                                                               */
+/*    Evaluate the validity of the scopes in the following expression         */
+/*    implementing stacks. Ignore the operators and operand in the expression */
+/*    while checking the validity of the expression.                          */
+/*                                                                            */
+/*    Comments: Write your comments or questions in red                       */
+/******************************************************************************/
+
+#include <stdio.h>
+
+void
+fileProcessing()
+{
+    FILE *fp;
+    fp = fopen("praveen-assignment-08-input.doc", "r");
+
+}
+
+
+
+int
+main(void) 
+{
+    fileProcessing();
+    return 0;
+}
