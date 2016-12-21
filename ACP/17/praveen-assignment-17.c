3658877051b88c20e799ca294545793fe12e87c2
Praveen Mhapsekar <praveenmhapsekar@gmail.com>
2016-12-20 17:03:11 -0800

    Assignment 17 infix to prefix solution

--- ACP/17/praveen-assignment-17.c
+++ ACP/17/praveen-assignment-17.c
@@ -19,104 +19,195 @@
 /*                                                                            */
 /*    Objective                                                               */
 /*        Use the functions PUSH, POP, INFIX, PREFIX_Convert, PREFIX_Eval     */
-/*		  and other required.                                                 */
+/*        and other required.                                                 */
 /*        Read the input into a buffer from input file.                       */
 /*        Place a space between numbers and operators, numbers and numbers,   */
 /*        and operators and operators. (Note: Do not initialize an array with */
 /*        input values.). Convert the following expression from infix notation*/ 
 /*        to prefix notation.                                                 */
+/*                                                                            */
 /*    Expression:                                                             */
-/*	(25 % 7 * 8) / (75 / 5 – 8 + (2 ^ 4 – (2 ^ 2 + 16) + 16 – 3) + 5 – 3 ^ 2) */
+/*        (25 % 7 * 8) / (75 / 5 - 8 + (2 ^ 4 - (2 ^ 2 + 16) + 16 - 3)        */
+/*         + 5 - 3 ^ 2)                                                       */
+/*                                                                            */
 /*        Using an array structure read the numbers from an input file.       */
-/*	      Print the original infix notation                                   */
+/*        Print the original infix notation                                   */
 /******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
-#include "binaryTree.h"
+#include <stdbool.h>
+#include "stack.h"
 
-#define FILE_IN  "praveen-assignment-13-input.doc"
-#define FILE_OUT "praveen-assignment-13-output.doc"
+#define FILE_IN  "praveen-assignment-17-input.doc"
+#define FILE_OUT "praveen-assignment-17-output.doc"
 
-void
-dataProcessing()
+char operator[7] = {'*','/','%','+','-','^'};
+char precedence[7] = {3,3,3,4,4,9};
+
+/*
+ * Check precedence between X and Y
+ *  return 1 if precedence(X) < precedence(y)
+ *  return 0 otherwise
+ */
+char
+checkPrecedance(char X, char Y)
 {
-	int  data;
-	char choice = 'y';
-    nodePtr tree = NULL;
-
-	/* Open input & output files */
-    finput = fopen(FILE_IN, "r");
-    fout = fopen(FILE_OUT, "w");
-
-	/* Read data from file and process it */
-	fprintf(fout, "Reading data from input file and inserting data into the tree :\n");
-    while (fscanf(finput, "%d", &data) == 1) {
-		fprintf(fout, "Insert %d\n", data);
-        insertTree(&tree, data);
+    int i;
+    char xPre = 0;
+    char yPre = 0;
+    for (i = 0; i <= 6; i++) {
+        if (X == operator[i]) {
+            xPre = precedence[i];
+            break;
+        }
     }
 
-	/* preOrder traversal */
-	fprintf(fout, "preOrder Traversal :\n");
-	preorder(tree);
-	fprintf(fout, "\n");
+    for (i = 0; i <= 6; i++) {
+        if (Y == operator[i]) {
+            yPre = precedence[i];
+            break;
+        }
+    }
 
-	/* inOrder traversal */
-	fprintf(fout, "inOrder Traversal :\n");
-	inorder(tree);
-	fprintf(fout, "\n");
+    if (xPre < yPre) {
+        return 1;
+    }
+    return 0;
+}
 
-	/* postOrder traversal */
-	fprintf(fout, "postOrder Traversal :\n");
-	postorder(tree);
-	fprintf(fout, "\n");
+/* 
+ *  Check if given token is opernad
+ */ 
+bool
+isOperand (char token) 
+{
+    if (((token >= '0') && (token <= '9')) ||
+        ((token >= 'a') && (token <= 'z')) ||
+        ((token >= 'A') && (token <= 'Z'))) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-	/* Accept user input and delete items from the tree */
-	while (choice == 'y') {
-		__fpurge(stdin);
-		printf ("Enter number to be deleted :\n");
-		fprintf (fout, "Enter number to be deleted :\n");
-		if (scanf("%d", &data) != 1) {
-			printf("Invalid  number, please re-enter!!!\n"); 
-			fprintf(fout, "%d", data);
-			fprintf(fout, "Invalid  number, please re-enter!!!\n"); 
-		} else {
-			/* Print user entered item to out file*/
-			fprintf(fout, "%d\n", data); 
+/* 
+ *  Check if given token is operator
+ */ 
+bool
+isOperator(char token)
+{
+    if ((token == '+') || 
+        (token == '-') ||
+        (token == '/') ||
+        (token == '*'))
+      return true;
+   else
+      return false;
+}   
 
-			/* Delete item from the tree */
-		 	if (!deleteTree(&tree, data)) {
-                fprintf(fout, "%d, not found in the tree\n", data);
-			} else {
-				fprintf(fout, "inOrder Traversal after deletion of %d\n", data);
-				inorder(tree);
-				fprintf(fout, "\n");
-			}
+/* 
+ *  eadck if given token is opernad
+ */ 
+void
+inputProcessing()
+{
+    char token;
+    char str[3];
+    char tmp;
+    FILE *fin;
+    FILE *fout;
+    stack_t *S;
+    stack_t *I;
+    stack_t *P; /* Operator stack */
+    int len, k = 0;
+    createStack(&I, sizeof(char)); 
+    createStack(&S, sizeof(char)); 
+    createStack(&P, sizeof(char)); 
+    /* Open input & output files */
+    fin  = fopen(FILE_IN, "r");
+    fout = fopen(FILE_OUT,"w");
 
-			/* Prompt user to continue */
-			while (1) {
-				__fpurge(stdin);
-				printf ("Do you want to continue (y/n)?");
-				if (scanf("%c", &choice) != 1) {
-					printf("Invalid choice\n");
-				} else {
-					break;
-				}
-			}
-		}
-	}
+    fprintf(fout, "\nRead expresseion from input file and print it\n");
+    while ((fscanf(fin, "%s", str) == 1)) {
+        k = 0;
+        len = strlen(str);
+        while (len) {
+            token = str[k++];
+            len--;
+            fprintf(fout, "%c", token);
+          //  if (token != ' ')
+               push(I, &token);
+        }
+        token = ' ';
+        push(I, &token);
+        fprintf(fout, "%c", token);
+    }
 
-	fprintf(fout, "\n");
+    fprintf(fout, "\n\n");
 
-	/* close files */
-    fclose(finput);
-	fclose(fout);
+    /* Read token from file and process it */
+    while (!isEmptyStack(I)) {
+        pop(I, &token);
+
+        switch (token) {
+            case ')' : push(S, &token);
+                       break;
+            case '(' :
+                      while (!isEmptyStack(S)) {
+                          pop(S, &tmp);
+                          if (tmp == '(')
+                              break;
+                          else
+                             push(P, &tmp);    
+                      }
+                      break;
+            case '+' :
+            case '-' :
+            case '/' :
+            case '*' :
+            case '^' : 
+            case '%' :          
+                      while (!isEmptyStack(S)) {
+                          peek(S, &tmp);
+                          if (checkPrecedance(token, tmp)) {
+                              pop(S, &tmp);
+                              push(P, &tmp);
+                          } else {
+                              break;
+                          }
+                      }
+                      push(S, &token);    
+                      break;
+            case ' ' :    
+                      push(P, &token);
+                      break;
+            default  :
+                      if(isOperand(token)) {
+                          push(P, &token);
+                      }           
+        }
+    }
+
+    fprintf(fout, "\nPrefix output is\n");
+    while(!isEmptyStack(P)) {
+         pop(P, &tmp);
+         if (tmp != ')')
+         fprintf(fout, "%c", tmp);
+    }     
+
+    fprintf(fout, "\n\n\n");
+
+    /* Close files */
+    fclose(fin);
+    fclose(fout);
 }
 
 int
 main(void) 
 {
-    dataProcessing();
+    inputProcessing();
     return 0;
 }
---
+++ ACP/17/stack.h
@@ -0,0 +1,35 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+typedef enum dataType {
+    signedchar,
+    unsignedChar,
+    signedShort,
+    unsignedShort,
+    unsignedInt,
+    signedInt,    
+    unsignedLong,
+    signedLong,
+    voidType,
+    dataTypeEnd,
+} dataType_t;
+
+typedef struct frame {
+    char *pData; 
+    struct frame *nxt;
+} frame_t;
+
+typedef struct stack {
+    int size;
+    int frameSize;
+    frame_t *top;
+} stack_t;
+
+int isEmptySatck(stack_t *S);
+int createStack(stack_t **, int);
+int destroyStack(stack_t **);
+int push(stack_t *, char *);
+int pop(stack_t *, char *);
+int peek(stack_t *, char *);
+
+#endif
